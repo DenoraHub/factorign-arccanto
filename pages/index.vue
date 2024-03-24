1a85a990add91def16b9d7d6ed291339f10b0d40
AlejandroWdno <alejandro.martinez@wdno.io>
2024-03-23 21:31:20 -0600

    connect prismic

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <prismic-rich-text :field="home.data.home_top_titlle"></prismic-rich-text>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { client } = usePrismic();
+
+const { data: home } = await useAsyncData("home", () =>
+    client.getByUID("homepage", "homepage")
+);
+
+</script>
